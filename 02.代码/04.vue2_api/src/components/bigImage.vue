515174b9a9c0d7c5676e662aec5c2c8415c56bc8
HotJuly <949588380@qq.com>
2023-01-30 16:56:33 +0800

    58.BigImage组件封装

---
+++ 02.代码/04.vue2_api/src/components/bigImage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <img :src="$attrs.src" alt="" srcset="">
+        <el-image v-bind="$attrs" v-show="false" ref="image" style="width: 100px; height: 100px" >
+        </el-image>
+        <!-- <el-button type="primary" @click="handler">放大</el-button> -->
+        <slot name="content" :listeners="{
+            click:handler
+        }"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'bigImage',
+    methods:{
+        handler(){
+            this.$refs.image.clickHandler();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
