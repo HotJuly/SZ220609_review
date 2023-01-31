e0eee9bf73d3177eff8ec987fa75923a7a351cc7
HotJuly <949588380@qq.com>
2023-01-31 13:28:14 +0800

    61.key值

--- 02.代码/04.vue2_api/src/App.vue
+++ 02.代码/04.vue2_api/src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
-     -->
-    <ul>
-      <li v-for="(item,index) in arr" :key="index">
-        <label>{{ item }}</label>
-        <input type="text">
-      </li>
-    </ul>
-    <button @click="handler">添加数字6</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -17,32 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data(){
-    return {
-      msg:123,
-      arr:[1,2,3,4,5]
-    }
-  },  
-  methods:{
-    handler(){
-      this.arr.splice(1,0,6);
-    }
-  },
   components: {
     HelloWorld
-  },
-  // beforeCreate(){
-  //   console.log('--------beforeCreate--------',this,this.$data,this.msg,this.$el)
-  // },
-  // created(){
-  //   console.log('--------created--------',this,this.$data,this.msg,this.$el)
-  // },
-  // beforeMount(){
-  //   console.log('--------beforeMount--------',this,this.$data,this.msg,this.$el)
-  // },
-  // mounted(){
-  //   console.log('--------mounted--------',this,this.$data,this.msg,this.$el)
-  // }
+  }
 }
 </script>
 
